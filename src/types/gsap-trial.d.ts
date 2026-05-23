40b26b3789fd86f8ea6f8ee483b24a03ce6aae1f
yhv <n m>
2026-05-24 01:27:49 +0500

    Fix GSAP production build issue

--- src/types/gsap-trial.d.ts
+++ src/types/gsap-trial.d.ts
@@ -1,16 +1,11 @@
-declare module "gsap-trial/SplitText" {
+declare module "gsap/SplitText" {
   const SplitText: any;
   export { SplitText };
   export default SplitText;
 }
 
-declare module "gsap-trial/ScrollSmoother" {
+declare module "gsap/ScrollSmoother" {
   const ScrollSmoother: any;
   export { ScrollSmoother };
   export default ScrollSmoother;
-}
-
-declare module "gsap-trial/*" {
-  const anyExport: any;
-  export default anyExport;
 }
--- src/types/gsap.d.ts
+++ src/types/gsap.d.ts
@@ -1,1 +1,4 @@
-import SplitText from "gsap-trial/SplitText";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
